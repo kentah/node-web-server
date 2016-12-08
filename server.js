1af196f275637f6ff4295298dd773660df2751e2
kentah <kenthow@gmail.com>
2016-12-07 18:47:30 -0600

    Create projects page

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/bad', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page',
+    projectsMessage: 'Some of a myriad of projects'
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`)
 });
